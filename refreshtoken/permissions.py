7d3f69d2ce8a55480573c5ff044a2d0565661b89
Daniel Hahler <github@thequod.de>
2018-06-19 03:36:09 +0200

    IsOwnerOrAdmin: improve user lookup and docs (#33)

--- refreshtoken/permissions.py
+++ refreshtoken/permissions.py
@@ -3,19 +3,19 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """
-    Only admins or owners can have permission
+    Only admins or owners are allowed.
     """
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated
+        user = request.user
+        return user and user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         """
-        If user is staff or superuser or 'owner' of object return True
-        Else return false.
+        Allow staff or superusers, and the owner of the object itself.
         """
-        if not request.user.is_authenticated:
+        user = request.user
+        if not user.is_authenticated:
             return False
-        elif request.user.is_staff or request.user.is_superuser:
+        elif user.is_staff or user.is_superuser:
             return True
-        else:
-            return request.user == obj.user
+        return user == obj.user
