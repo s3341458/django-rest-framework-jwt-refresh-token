e8516947de15e3b1524b51c379aa33f453883c02
cheng <cheng@commoncode.io>
2019-04-26 16:45:18 +1000

    support basic admin

---
+++ refreshtoken/admin.py
@@ -0,0 +1,34 @@
+from .models import RefreshToken
+
+from django.contrib import admin
+from django.conf import settings
+
+from django.apps import apps
+
+
+# Prior to Django 1.5, the AUTH_USER_MODEL setting does not exist.
+# Note that we don't perform this code in the compat module due to
+# bug report #1297
+# See: https://github.com/tomchristie/django-rest-framework/issues/1297
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+User = apps.get_model(*AUTH_USER_MODEL.split('.', 1))
+
+
+def revoke_refresh_tokens(modelAdmin, request, queryset):
+    for token in queryset:
+        token.revoke()
+
+
+revoke_refresh_tokens.short_description = 'Revoke selected tokens'
+
+
+@admin.register(RefreshToken)
+class RefreshTokenAdmin(admin.ModelAdmin):
+    readonly_fields = ['user', 'key', 'app']
+    list_display = ['user', 'key']
+    actions = [revoke_refresh_tokens, ]
+
+    ordering = search_fields = ['user__' + User.USERNAME_FIELD]
